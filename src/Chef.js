f8f42be28cabd7f04bf1c30c698b9a97f11675bd
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-10-11 12:53:18 -0600

    Add chef.js file

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,23 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurant) {
+    this.name = chefName;
+    this.restaurant = restaurant;
   }
+
+  greetCustomer(customerName, isMorning) {
+    if (isMorning) {
+      return `Good morning, ${customerName}!`
+  }
+    return `Hello, ${customerName}!`
+  }
+
+  checkForFood(foodItem) {
+    if (this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    }
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+  }
+
 }
 
 module.exports = Chef;
