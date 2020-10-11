341dfb81203a7f3bfbf7d7c2c03f3f95c57edfea
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-10-11 12:32:35 -0600

    Add order.js file

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
 
+function takeOrder(orderNum, deliveryOrders) {
+    if (deliveryOrders.length < 3) {
+      deliveryOrders.push(orderNum);
+    }
+  return deliveryOrders;
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderNum === deliveryOrders[i].orderNumber) {
+      deliveryOrders.splice(i,1)
+    }
+  }
+    return deliveryOrders
+}
+
+function listItems(deliveryOrders) {
+  var paragraph = ''
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    var sentence = `${deliveryOrders[i].item}, `
+    paragraph += sentence;
+  }
+  return paragraph.replace(/,\s*$/, "")
+}
+
+function searchOrder(deliveryOrders, item) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+      if (item === deliveryOrders[i].item) {
+        return true;
+      }
+  }
+      return false
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
