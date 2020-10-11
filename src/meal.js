5ad2c3ab9b4ece915b5a5933f6a19f5cd5c7c838
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-10-11 11:47:59 -0600

    Add meal.js file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,52 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`
+}
+
+function createMenuItem(foodItemName, price, type) {
+
+var menuItem = {
+  name: foodItemName,
+  price: price,
+  type: type
+}
+return menuItem;
+}
+
+function addIngredients(ingredientName, ingredients) {
+  if (ingredients.includes(ingredientName)){
+      return
+  } else {
+    ingredients.push(ingredientName);
+  }
+  return ingredients;
+}
+
+
+function formatPrice(initialPrice) {
+    return `$${initialPrice}`;
+}
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice * .90
+}
+
+function createRecipe(title, ingredientName, menuType) {
+
+var recipe = {
+  title: title,
+  ingredients: ingredientName,
+  type: menuType
+}
+return recipe;
 
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+   addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
