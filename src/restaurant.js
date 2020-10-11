6085ba64a0c567421dca7d33495603891711e6b9
Kelsie Besinger <kelsiebesinger@gmail.com>
2020-10-11 12:46:03 -0600

    Add restaurant.js file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,40 @@
+function createRestaurant(name, menus) {
 
+  var restaurant = {
+    name: name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  }
+  return restaurant;
+}
+
+function addMenuItem(restaurantName, menuItem) {
+
+    var menuArray = restaurantName.menus[menuItem.type]
+    if (menuArray.includes(menuItem)) {
+    } else {
+      menuArray.push(menuItem);
+    }
+}
+
+
+function removeMenuItem(restaurantName, menuItem, menuType) {
+
+  var menuTypeArray = restaurantName.menus[menuType]
+  for (var i = 0; i < menuTypeArray.length; i++) {
+      if (menuTypeArray[i].name === menuItem) {
+        menuTypeArray.splice(i,1);
+        return `No one is eating our ${menuItem} - it has been removed from the ${menuType} menu!`
+      }
+  }
+  return `Sorry, we don\'t sell ${menuItem}, try adding a new recipe!`
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
